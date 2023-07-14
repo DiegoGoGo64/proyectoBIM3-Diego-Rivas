50cd4670443747234d1cfa25eae1f245ff1eaeb1
DiegoGOGO64 <hola.dia.6789@gmail.com>
2023-07-13 21:01:40 -0600

    Terminando la clase Contacto

--- agendaTelefonica/src/agendatelefonica/Contacto.java
+++ agendaTelefonica/src/agendatelefonica/Contacto.java
@@ -1,5 +1,15 @@
 package agendatelefonica;
 
 public class Contacto {
-    
+
+    private String numero;
+    private String nombre;
+    private String domicilio;
+
+    public Contacto(String numero, String nombre, String domicilio) {
+        this.numero = numero;
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+    }
+
 }
