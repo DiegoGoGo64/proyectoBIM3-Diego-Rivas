69da35dfab207c8c9177babdf78602e2c0448be7
DiegoGoGo64 <138635353+DiegoGoGo64@users.noreply.github.com>
2023-07-14 21:32:17 -0600

    Agregando menú de opciones y contador al código principal.

--- agendaTelefonica/src/agendatelefonica/AgendaTelefonica.java
+++ agendaTelefonica/src/agendatelefonica/AgendaTelefonica.java
@@ -3,7 +3,17 @@ package agendatelefonica;
 public class AgendaTelefonica {
 
     public static void main(String[] args) {
+        int opcion = 0;
+        Scanner entrada = new Scanner(System.in);
 
+        System.out.println("|     AGENDA TELEFONICA     |");
+        System.out.println("1. Guardar contacto");
+        System.out.println("2. Ver todos los contactos");
+        System.out.println("3. Buscar un contacto");
+        System.out.println("4. Salir");
+        System.out.println("Ingrese el numero de opcion: ");
+        opcion = entrada.nextInt();
+        
     }
     
 }
